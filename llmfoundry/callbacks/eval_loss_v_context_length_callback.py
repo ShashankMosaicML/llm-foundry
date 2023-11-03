ea4f014d09e94f26c15ef356bc0b9e77c535d5bb
Shashank Rajput <shashank.rajput@databricks.com>
2023-11-03 20:34:21 +0000

    ..

--- llmfoundry/callbacks/eval_loss_v_context_length_callback.py
+++ llmfoundry/callbacks/eval_loss_v_context_length_callback.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import torch
+import wandb
 from composer.core import Callback, State
 from composer.loggers import Logger
 
@@ -9,7 +10,6 @@ from composer.loggers import Logger
 class LossVsContextLengthEvaluator(Callback):
 
     def eval_batch_end(self, state: State, logger: Logger) -> None:
-        del logger  # unused
         perplexity = state.eval_metrics['eval'][
             'LanguagePerplexityNoReduce'].compute()
         seq_id = state.batch['sequence_id']
@@ -33,5 +33,10 @@ class LossVsContextLengthEvaluator(Callback):
             'LanguagePerplexityNoReduce'].sum_perp = sum_perp
         state.eval_metrics['eval'][
             'LanguagePerplexityNoReduce'].sum_length = sum_length
-        print(sum_length)
-        print(sum_perp / sum_length)
+        # print(sum_length)
+        # print(sum_perp / sum_length)
+        sum_perp = torch.where(sum_length!=0, sum_perp, -1)
+        sum_length = torch.where(sum_length!=0, sum_length, 1)
+        avg_perp = sum_perp / sum_length
+        scatter_lcp = wandb.Table(data=[[i, b] for (i, b) in enumerate(avg_perp.tolist())], columns = ["seq_length", "perplexity"])
+        wandb.log({f"long_context_perplexity": wandb.plot.scatter(scatter_lcp, "seq_length", "perplexity", title=f"long_context_perplexity")}, step=state.timestamp.batch.value)
