9fe49e594db75aefcfc44dad9943f1b9dfe92e06
Shashank Rajput <shashank.rajput@databricks.com>
2023-11-04 05:33:02 +0000

    ..

--- llmfoundry/callbacks/eval_loss_v_context_length_callback.py
+++ llmfoundry/callbacks/eval_loss_v_context_length_callback.py
@@ -11,15 +11,14 @@ from composer.utils import dist
 class LossVsContextLengthEvaluator(Callback):
 
     def eval_batch_end(self, state: State, logger: Logger) -> None:
+        sum_perp, sum_length = state.eval_metrics['eval']['LanguagePerplexityNoReduce'].compute()
+        sum_perp = torch.where(sum_length != 0, sum_perp, -1)
+        sum_length = torch.where(sum_length != 0, sum_length, 1)
+        avg_perp = sum_perp / sum_length
+        scatter_lcp = wandb.Table(
+            data=[[i, b] for (i, b) in enumerate(avg_perp.tolist())],
+            columns=['seq_length', 'perplexity'])
         if dist.get_local_rank() == 0:
-            sum_perp, sum_length = state.eval_metrics['eval'][
-                'LanguagePerplexityNoReduce'].compute()
-            sum_perp = torch.where(sum_length != 0, sum_perp, -1)
-            sum_length = torch.where(sum_length != 0, sum_length, 1)
-            avg_perp = sum_perp / sum_length
-            scatter_lcp = wandb.Table(
-                data=[[i, b] for (i, b) in enumerate(avg_perp.tolist())],
-                columns=['seq_length', 'perplexity'])
             wandb.log(
                 {
                     f'long_context_perplexity':
