da4325f25431ca748c6e741ddd2804c8d2448189
Shashank Rajput <shashank.rajput@databricks.com>
2023-12-16 17:29:10 +0000

    ..

--- llmfoundry/models/mpt/modeling_mpt.py
+++ llmfoundry/models/mpt/modeling_mpt.py
@@ -482,7 +482,6 @@ class MPTModel(MPTPreTrainedModel):
         query_long_dist_lin = (2*tok_id/(2*ratio_q_kv).permute(0,2,1)[:, :, None, :])*((1+2*torch.arange(ratio_q_kv))[None, None, :, None])
         query_long_dist = torch.stack([query_long_dist_quad, query_long_dist_lin], dim=-1).to(query_alibi)
 
-        breakpoint()
         return {'query_alibi': query_alibi, 'key_alibi': key_alibi, 'query_long_dist': query_long_dist, 'key_long_dist': key_long_dist}
         
 
