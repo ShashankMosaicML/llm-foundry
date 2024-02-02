43bb70dcad34ce652ee9ae93303ae876d09b57ff
Shashank Rajput <shashank.rajput@databricks.com>
2024-02-02 00:45:40 +0000

    ..

--- scripts/train/train.py
+++ scripts/train/train.py
@@ -590,7 +590,7 @@ def main(cfg: DictConfig) -> Trainer:
     # Now add the eval metrics
     if eval_loader_config is not None and not use_async_eval:
         train_metrics = model.get_metrics(is_train=True)
-        if model_config.get("eval_loss_v_len", False):
+        if model_config.get('eval_loss_v_len', False):
             eval_metrics = model.get_metrics(is_train=False)
             if 'LossPerpVLen' in eval_metrics:
                 train_metrics['LossPerpVLen'] = eval_metrics['LossPerpVLen']
